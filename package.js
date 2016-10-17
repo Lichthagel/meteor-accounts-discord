b3fd52f6445e6a44424d8a1932f272be0614151f
Lichthagel <lichthagel@gmail.com>
2016-10-17 11:53:08 +0200

    Oops wrong one

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lichthagel:accounts-discord',
-  version: '0.1.3',
+  version: '0.1.4',
   // Brief, one-line summary of the package.
   summary: 'Adds account support for Discord',
   // URL to the Git repository containing the source code for this package.
